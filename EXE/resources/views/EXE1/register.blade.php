06ac3f4ef7011f5e387cd87b7ef10612e07ac937
nguyenhuuhaohung <nguyenhaohung640@gmail.com>
2025-04-04 13:50:34 +0700

    update file

--- EXE/resources/views/EXE1/register.blade.php
+++ EXE/resources/views/EXE1/register.blade.php
@@ -22,6 +22,8 @@
 
     <!-- Login -->
     <div class="container">
+      <form action="{{ route('user.postUser') }}" method="POSt">
+        @csrf
       <table
         class="table m-auto"
         style="table-layout: auto; width: auto; border: 1px solid #ccc"
@@ -33,25 +35,25 @@
           <tr>
             <td>Username</td>
             <td>
-              <input type="text" name="username" placeholder="Username"/>
+            <input type="text" placeholder="Name" id="name" class="form-control" name="name" required autofocus>
             </td>
           </tr>
           <tr>
             <td>Mật khẩu</td>
             <td>
-              <input type="text" name="password" placeholder="Password"/>
+            <input type="password" placeholder="Password" id="password" class="form-control" name="password" required autofocus>
             </td>
           </tr>
           <tr>
             <td>Nhập lại mật khẩu</td>
             <td>
-              <input type="text" name="Re_enter_password" placeholder="Re-enter password" />
+            <input type="password" placeholder="Re_entry password" id="password" class="form-control" name="password" required autofocus>
             </td>
           </tr>
           <tr>
             <td>Email</td>
             <td>
-              <input type="text" name="email" placeholder="Email"/>
+            <input type="email" placeholder="Email" id="email" class="form-control" name="email" required autofocus>
             </td>
           </tr>
           <tr class="pt-4">
@@ -63,6 +65,8 @@
           </tr>
         </tbody>
       </table>
+      </form>
+      
     </div>
 
     <!-- Footer -->
