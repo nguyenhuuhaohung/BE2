b24d908a39d7dc4f91284b35656f5c535b33e9bb
nguyenhuuhaohung <nguyenhaohung640@gmail.com>
2025-04-04 13:49:23 +0700

    update file

--- EXE/database/migrations/2014_10_12_000000_create_users_table.php
+++ EXE/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('address');
-            $table->integer('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
